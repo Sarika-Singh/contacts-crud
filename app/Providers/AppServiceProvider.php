d9e880e508e8eb8dd48b9f2449f01855f41fda52
Sarika-Singh <sarikasinghr20@gmail.com>
2025-09-23 01:44:39 +0530

    Code-Cleanup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,17 +8,11 @@ use App\Repositories\ContactRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         $this->app->bind(ContactRepositoryInterface::class, ContactRepository::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -8,9 +8,6 @@ use App\Models\Contact;
 
 class ContactSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         Contact::factory()->count(10)->create();
